494f3af630ee26009a72bcd3259e66b229efbbcf
Damian Nolan <damiannolan@gmail.com>
2017-11-13 21:27:41 +0000

    setup basic route for uploading a file and saving it to the route dir

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/upload', methods = ['POST'])
+def upload_file():
+    file = request.files['file']
+    file.save((file.filename))
+    return jsonify('file saved')
+
 if __name__ == '__main__':
     app.run()
