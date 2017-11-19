08654d566e5acc62cc8397d3ab212cf2ae13ade7
Damian Nolan <damiannolan@gmail.com>
2017-11-19 18:41:19 +0000

    converting request image to grayscale and resizing to 28x28. updated gitignore

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+from PIL import Image
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/upload', methods = ['POST'])
 def upload_file():
-    file = request.files['file']
-    file.save((file.filename))
-    return jsonify('file saved')
+    img = Image.open(request.files['file'].stream).convert('L')
+    img = img.resize((28, 28))
+    img.show()
+    img.save('img/resize.png')
+
+    prediction = 4
+    return jsonify(str(prediction))
 
 if __name__ == '__main__':
     app.run()
