942f27da205fc95f3dd7ececc620399f7ec52e98
Damian Nolan <damiannolan@gmail.com>
2017-11-19 18:58:58 +0000

    flatten img arrays from 28x28 to 784 using reshape and convert to float32

--- keras-mnist.py
+++ keras-mnist.py
@@ -5,3 +5,11 @@ import keras as kr
 # The data, shuffled and split between train and test sets
 # x_train, x_test - image data. y_train, y_test - labels
 (x_train, y_train), (x_test, y_test) = kr.datasets.mnist.load_data()
+
+# x_train and x_test are 3 dim with shape (60000, 28, 28) and (10000, 28, 28)
+# Flatten x_train and x_test to 2 dim with shape (60000, 784) and (10000, 784)
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+# The mnist import data is of type of uint8 so must be converted to float32 for use with Keras
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
