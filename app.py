019519af303312226239fb5da16162894fe4bec7
Damian Nolan <damiannolan@gmail.com>
2017-11-20 03:03:48 +0000

    cleaned up

--- app.py
+++ app.py
@@ -8,30 +8,29 @@ import numpy as np
 app = Flask(__name__)
 CORS(app)
 
+# Load the model from the h5 file
+# Pip3 install h5py
 model = kr.models.load_model('mnist-neural.h5')
 
+# Print the model summary to the terminal
 model.summary()
 
 @app.route('/upload', methods = ['POST'])
 def upload_file():
+    # Load the image file into an object using PIL Image lib
+    # Use .convert('L') to convert the file to (8-bit pixels, black and white) - grayscale
+    # Use .resize() to resize the width & height to 28 x 28
     img = Image.open(request.files['file'].stream).convert('L')
     img = img.resize((28, 28))
-    #img.show()
-    pix = np.array(img).astype(np.float32)
-    print(pix)
-    print(pix.shape)
-    print(pix.dtype)
-
-    pix = pix.reshape(784)
-    #pix /= 255
-    print(pix)
-    print(pix.shape)
-    #print(img.tobytes())
-    #img.save('img/resize.png')
     
-    test_image = np.array([pix])
-    prediction = list(model.predict(test_image))
-    print(prediction[0])
+    # Convert the PIL Image to a pixel array by passing it to np.array()
+    # And change the dtype to np.float32
+    pixel_array = np.array(img).astype(np.float32)
+    pixel_array = pixel_array.reshape(784)
+    
+    # The models accepts an array of images to make predictions, so will return an array of predictions
+    # Use np.argmax() to find the prediction value in the One-Hot vector
+    prediction = list(model.predict(np.array([pixel_array])))
     prediction = np.argmax(prediction[0])
 
     return jsonify(str(prediction))
